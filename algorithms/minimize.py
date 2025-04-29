babf3e5ba945174c49b98e4440d2b93a9e1afb9a
Andrea Tangherloni <andrea.tangherloni@unibocconi.it>
2025-04-29 18:25:34 +0200

    backward ...

--- algorithms/minimize.py
+++ algorithms/minimize.py
@@ -3,12 +3,18 @@ def minimize(algorithm, max_evals=10000, verbose=True):
     generation = 1
     while algorithm.n_evals < max_evals:
         
+        algorithm.optimizer.zero_grad(set_to_none=True)
+        
         # (1) Forward pass => builds graph, returns candidate best fitness (scalar)
         loss = algorithm()
+                
+        # 2) back-prop through the whole generation
+        loss.backward() 
         
-        # (2) Backprop on the scalar loss  
-        algorithm.optimizer.zero_grad(set_to_none=True)
+        # 3) exploitation / hyper-learning step
         algorithm.optimizer.step()
+        
+        # 4) commit the evolutionary changes
         algorithm.update_state()
                         
         if verbose:
